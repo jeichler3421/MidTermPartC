1729140c051c3e5fb1cf747bd2959cb4414fc342
Jack <jeichler3421@mail.com>
2023-02-21 10:50:11 -0500

    Added Jack.java to store all the comments made

---
+++ src/midtermexam_versiona_extensioncode/Jack.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author jeich
+ */
+public class Jack {
+    
+    //This class is for comments
+}
